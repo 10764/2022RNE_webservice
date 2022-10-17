78f63488aca8ee765e2c7c7a090a55b40057e8bf
Mangnyangnyang <109772410+Mangnyangnyang@users.noreply.github.com>
2022-10-17 20:30:12 +0900

    sample

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
-st.title('상관관계 : 유흥주점과 강력범죄율')
+st.title('연구결과')
+st.write('ㅋㅋㅋ')
 
 data = pd.read_csv('유흥주점비율.csv')
 img = Image.open('graph.png')
