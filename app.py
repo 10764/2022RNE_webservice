336ca05b67101c0507ff3f495bcfc187f3d311fa
Mangnyangnyang <109772410+Mangnyangnyang@users.noreply.github.com>
2022-10-17 20:25:26 +0900

    sample

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 st.title('상관관계 : 유흥주점과 강력범죄율')
 
 data = pd.read_csv('유흥주점비율.csv')
-img = Image.open('Mangnyangnyang/RNE_sample/KakaoTalk_20221017_132830006.png')
+img = Image.open('graph.png')
 
 st.dataframe(data)
 st.image(img)
