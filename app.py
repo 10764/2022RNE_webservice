746971603f00c21510931b623006e3b8c97b6fc3
Mangnyangnyang <109772410+Mangnyangnyang@users.noreply.github.com>
2022-10-18 17:40:02 +0900

    app

--- app.py
+++ app.py
@@ -5,11 +5,24 @@ from PIL import Image
 st.title('연구결과')
 
 with st.sidebar :
-    gubun = st.radio("열람하고 싶은 결과를 선택하세요.", ("토지 및 인구 관련 요인", "주민 유형", "치안 관련 요인", "건물 유형"))
+    gubun = st.radio("열람하고 싶은 결과를 선택하세요.", ("강력범죄간 상관관계", "토지 및 인구 관련 요인", "주민 유형", "치안 관련 요인", "건물 유형"))
 
+if gubun == "강력범죄간 상관관계" :
+    st.title("강력범죄간 상관관계")
+    st.write("둠치기박치기1")
 
-data = pd.read_csv('유흥주점비율.csv')
-img = Image.open('graph.png')
+elif gubun == "토지 및 인구 관련 요인" :
+    st.title("토지 및 인구 관련 요인")
+    st.write("둠치기박치기2")
 
-st.dataframe(data)
-st.image(img)
+elif gubun == "주민 유형" :
+    st.title("주민 유형")
+    st.write("둠치기박치기3")
+
+elif gubun == "치안 관련 요인" :
+    st.title("치안 관련 요인")
+    st.write("둠치기박치기4")
+
+else :
+    st.title("건물 유형")
+    st.write("둠치기박치기5")
