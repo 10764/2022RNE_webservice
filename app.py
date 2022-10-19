6b84dd5aaf8d6ab9a08eb2a418aba70e2b8b58cd
Mangnyangnyang <109772410+Mangnyangnyang@users.noreply.github.com>
2022-10-19 17:46:46 +0900

    app

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ if gubun == "강력범죄간 상관관계" :
     st.title("강력범죄간 상관관계")
     t1, t2 = st.tabs(["범죄 수", "범죄율"])
     with t1 :
-        data = pd.read_csv('범죄간상관관계.csv')
+        data = pd.read_csv('범죄간상관관계.csv', encoding = 'cp949')
         st.dataframe(data)
         image = Image.open('범죄간상관관계사진.png')
         st.image(image, caption = '강력범죄간 상관관계')
