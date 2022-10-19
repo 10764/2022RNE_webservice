f004d9792a6c40fe4caa5958649ca7208a4d6545
Mangnyangnyang <109772410+Mangnyangnyang@users.noreply.github.com>
2022-10-19 17:17:59 +0900

    app

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ with st.sidebar :
     gubun = st.radio("열람하고 싶은 결과를 선택하세요.", ("강력범죄간 상관관계", "토지 및 인구 관련 요인", "주민 유형", "치안 관련 요인", "건물 유형"))
 
 if gubun == "강력범죄간 상관관계" :
-    st.title("강력범죄간 상관관계")
+    t1, t2 = st.tabs(["범죄 수", "범죄율"])
 
 elif gubun == "토지 및 인구 관련 요인" :
     st.title("토지 및 인구 관련 요인")
@@ -14,12 +14,12 @@ elif gubun == "토지 및 인구 관련 요인" :
 
 elif gubun == "주민 유형" :
     st.title("주민 유형")
-    st.write("둠치기박치기3")
+    t1, t2, t3 = st.tabs(["연령구간별 인구수", "세대당 인구", "기초생활수급자 수"])
 
 elif gubun == "치안 관련 요인" :
     st.title("치안 관련 요인")
-    st.write("둠치기박치기4")
+    t1, t2 = st.tabs(["CCTV 개수", "치안센터 수"])
 
 else :
     st.title("건물 유형")
-    st.write("둠치기박치기5")
+    t1, t2, t3, t4 = st.tabs(["유치원", "학교", "학원", "유흥주점"])
